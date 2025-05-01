ab01cf288f2b96d8b096e39e9ca33d0883c61707
Aleshkevich Pavel <aleskevic79@gmail.com>
2025-05-01 17:26:25 +0300

    init commit

---
+++ Movies.Api/Program.cs
@@ -0,0 +1,34 @@
+using Movies.Application;
+using Movies.Application.Repositories;
+
+namespace Movies.Api;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddApplication();
+        
+        // Add services to the container.
+        builder.Services.AddAuthorization();
+
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        builder.Services.AddOpenApi();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.Run();
+    }
+}
